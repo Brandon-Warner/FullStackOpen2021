96bcb0cc4d819b9046ff174994bed3a7cbe7f70f
Brandon Warner <brandonswarner5@gmail.com>
2021-04-22 19:20:17 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "111-222-1212",
+  },
+  {
+    id: 2,
+    name: "Ben Franklin",
+    number: "333-444-3434",
+  },
+  {
+    id: 3,
+    name: "Damian Lillard",
+    number: "000-000-0000",
+  },
+  {
+    id: 4,
+    name: "Marcus Mariota",
+    number: "888-888-8888",
+  },
+];
+
+app.get("/info", (request, response) => {
+  const info = `Phonebook has ${persons.length} contacts`;
+  const date = new Date();
+
+  const allInfo = `<p> ${info} </p> <br> <p>${date}</p>`;
+
+  response.send(allInfo);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const newId = () => {
+  const id = Math.floor(Math.random() * 1000);
+  return id;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+  if (!body) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+  const person = {
+    id: newId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  console.log("person =", person);
+
+  persons = persons.concat(person);
+
+  response.json(persons);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
