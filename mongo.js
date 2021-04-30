c715bad9a52a4d57932816a2d0bf67f3512c7771
Brandon Warner <brandonswarner5@gmail.com>
2021-04-29 17:42:23 -0700

    Functional mongo.js file for adding or displaying people to phonebook

--- mongo.js
+++ mongo.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
 
-if (process.argv.length < 5 && process.argv.length > 3) {
-  console.log(
-    "Please provide password, name, and number as an arg: node mongo.js <password> -NAME-  -NUMBER-"
-  );
-  process.exit(1);
-}
-
 const password = process.argv[2];
 const name = process.argv[3];
 const number = process.argv[4];
@@ -28,13 +21,35 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model("Person", personSchema);
 
-const person = new Person({
-  id: Math.floor(Math.random() * 1000),
-  name: name,
-  number: number,
-});
+if (process.argv.length === 5) {
+  const person = new Person({
+    id: Math.floor(Math.random() * 1000),
+    name: name,
+    number: number,
+  });
 
-person.save().then((result) => {
-  console.log(`added ${person.name} ${person.number} to the phonebook`);
-  mongoose.connection.close();
-});
+  person.save().then((result) => {
+    console.log(`added ${person.name} ${person.number} to the phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (
+  process.argv.length === 4 ||
+  process.argv.length > 5 ||
+  process.argv.length < 3
+) {
+  console.log(
+    "Please provide password, name, and number as an arg: node mongo.js <password> -NAME-  -NUMBER-"
+  );
+  process.exit(1);
+}
