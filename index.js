401591e257b0515e339e3ec9612e43ebb19e97d6
Brandon Warner <brandonswarner5@gmail.com>
2021-05-06 15:33:33 -0700

    Added delete request functionality

--- index.js
+++ index.js
@@ -41,23 +41,51 @@ mongoose
 app.use(cors())
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+    console.log('Method: ', request.method)
+    console.log('Path: ', request.path)
+    console.log('Body: ', request.body)
+    console.log('---')
+    next()
+}
+app.use(requestLogger)
+
 app.get('/api/blogs', (request, response) => {
     Blog.find({}).then((blogs) => {
         response.json(blogs)
     })
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post('/api/blogs', (request, response, next) => {
     const blog = new Blog(request.body)
 
     blog.save()
         .then((result) => {
-            response.status(201).json(result)
+            response.json(result)
         })
         .catch((error) => {
             console.log(error.message)
+            next()
         })
 })
+
+app.delete('/api/blogs/:id', (request, response, next) => {
+    Blog.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch((error) => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.message === 'ValidationError') {
+        response.status(400).send({ error: 'invalid request' })
+    }
+    
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
