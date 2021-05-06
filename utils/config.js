bff26a5d60920dba6cc855b5cd7503a497ee14b2
Brandon Warner <brandonswarner5@gmail.com>
2021-05-06 16:26:54 -0700

    Fixed config.js file

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
-const MONGODB_URI =
-    'mongodb+srv://brandonwarner5:HSF21@cluster0.m0bxt.mongodb.net/bloglist-app?retryWrites=true&w=majority'
+const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
     MONGODB_URI,
