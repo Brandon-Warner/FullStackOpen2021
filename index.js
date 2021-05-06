369d4e4fca82bee39030f26716537d49bf19de48
Brandon Warner <brandonswarner5@gmail.com>
2021-05-06 16:46:54 -0700

    app.js file created -- still need to clean up index.js file

--- index.js
+++ index.js
@@ -10,24 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 
-const requestLogger = (request, response, next) => {
-    logger.info('Method: ', request.method)
-    logger.info('Path: ', request.path)
-    logger.info('Body: ', request.body)
-    logger.info('---')
-    next()
-}
-app.use(requestLogger)
 
-const errorHandler = (error, request, response, next) => {
-    logger.error(error.message)
-    if (error.message === 'ValidationError') {
-        response.status(400).send({ error: 'invalid request' })
-    }
-
-    next(error)
-}
-app.use(errorHandler)
 
 app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
