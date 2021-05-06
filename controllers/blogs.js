0f4a37acc890f39e7ef05ba7564ea6e6fea84789
Brandon Warner <brandonswarner5@gmail.com>
2021-05-06 16:01:02 -0700

    Generated blogs.js file for all routes. Have not deleted routes from original index.js

---
+++ controllers/blogs.js
@@ -0,0 +1,32 @@
+// contain all of the routes
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+    const blog = new Blog(request.body)
+
+    blog.save()
+        .then((result) => {
+            response.json(result)
+        })
+        .catch((error) => {
+            console.log(error.message)
+            next()
+        })
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+    Blog.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch((error) => next(error))
+})
+
+module.exports = blogsRouter
