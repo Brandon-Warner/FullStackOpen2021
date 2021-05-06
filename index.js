f083c3793fb791cad3c4eba0b9038180bc286f27
Brandon Warner <brandonswarner5@gmail.com>
2021-05-06 16:05:03 -0700

    functional router file blogs.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
 const requestLogger = (request, response, next) => {
     console.log('Method: ', request.method)
@@ -17,33 +17,6 @@ const requestLogger = (request, response, next) => {
     next()
 }
 app.use(requestLogger)
-
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response, next) => {
-    const blog = new Blog(request.body)
-
-    blog.save()
-        .then((result) => {
-            response.json(result)
-        })
-        .catch((error) => {
-            console.log(error.message)
-            next()
-        })
-})
-
-app.delete('/api/blogs/:id', (request, response, next) => {
-    Blog.findByIdAndRemove(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch((error) => next(error))
-})
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
