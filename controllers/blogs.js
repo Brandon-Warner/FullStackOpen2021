b6983169c0ccfc0262e2d129a00e818820cd1900
Brandon Warner <brandonswarner5@gmail.com>
2021-05-12 12:14:51 -0700

    blog likes can be updated with HTTP PUT

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -20,6 +20,22 @@ blogsRouter.post('/', async (request, response) => {
     response.json(savedBlog.toJSON())
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0,
+        id: body.id,
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+        new: blog.likes,
+    })
+    response.json(updatedBlog)
+})
+
 blogsRouter.delete('/:id', async (request, response) => {
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
