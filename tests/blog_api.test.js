f7226d8ac735ad74db3674269275045fa37dca5b
Brandon Warner <brandonswarner5@gmail.com>
2021-05-20 10:36:15 -0700

    Added test_helper file for containing consts needed for tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,42 +1,15 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const helper = require('./test_helper')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
-const initialBlogs = [
-    {
-        title: 'Ducks For Life',
-        author: 'Tucker Legg',
-        url: 'www.ilovemyducks.com',
-        likes: 15,
-        id: '609ace54a851f5066ec1efc7',
-    },
-    {
-        title: 'Blazer Fans4U',
-        author: 'Wells Fryan',
-        url: 'www.gozers.com',
-        likes: 10,
-        id: '609ace54a851f5066ec1efc8',
-    },
-    {
-        title: 'Seattle United',
-        author: 'Shawn Zayne',
-        url: 'www.pudgetcrown.com',
-        likes: 12,
-        id: '609ace54a851f5066ec1efc9',
-    },
-]
+const User = require('../models/user')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
-    await blogObject.save()
-    blogObject = new Blog(initialBlogs[2])
-    await blogObject.save()
+    await Blog.insertMany(helper.initialBlogs)
 })
 
 test('blogs are returned as json', async () => {
@@ -48,7 +21,7 @@ test('blogs are returned as json', async () => {
 
 test('there are 3 blogs returned', async () => {
     const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('all blogs have an id', async () => {
@@ -77,7 +50,7 @@ test('successfully adding a blog', async () => {
 
     const titles = response.body.map((r) => r.title)
 
-    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
     expect(titles).toContain('Practice blog')
 })
 
@@ -114,7 +87,7 @@ test('blog will be rejected if no title', async () => {
 
     const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(initialBlogs.length)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('blog will be rejected if no url', async () => {
@@ -130,7 +103,7 @@ test('blog will be rejected if no url', async () => {
 
     const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(initialBlogs.length)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 afterAll(() => {
