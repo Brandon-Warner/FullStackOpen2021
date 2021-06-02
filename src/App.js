55d3286e0483e08fc29e320c42a12b9cc82fceaf
Brandon Warner <brandonswarner5@gmail.com>
2021-06-02 14:11:01 -0700

    created login form

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+    const [blogs, setBlogs] = useState([])
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    useEffect(() => {
+        blogService.getAll().then(blogs => setBlogs(blogs))
+    }, [])
+
+    const handleLogin = event => {
+        event.preventDefault()
+        console.log('logging in with', username, password)
+    }
+
+    return (
+        <div>
+            <h2>login</h2>
+            <form onSubmit={handleLogin}>
+                <div>
+                    username
+                    <input
+                        type='text'
+                        value={username}
+                        name='Username'
+                        onChange={({ target }) => setUsername(target.value)}
+                    />
+                    <br></br>
+                    password
+                    <input
+                        type='text'
+                        value={password}
+                        name='Password'
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                </div>
+                <button type='submit'>login</button>
+            </form>
+
+            <h2>blogs</h2>
+            {blogs.map(blog => (
+                <Blog key={blog.id} blog={blog} />
+            ))}
+        </div>
+    )
+}
+
+export default App
