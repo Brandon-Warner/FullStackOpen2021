b5667150adfc04a08ac57636afae1afe38fbdbe9
Brandon Warner <brandonswarner5@gmail.com>
2021-06-03 13:28:46 -0700

    added login conditionals

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
 
     useEffect(() => {
         blogService.getAll().then(blogs => setBlogs(blogs))
     }, [])
 
-    const handleLogin = event => {
+    const handleLogin = async event => {
         event.preventDefault()
         console.log('logging in with', username, password)
+        try {
+            const user = await loginService.login({
+                username,
+                password,
+            })
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {}
+    }
+    if (user === null) {
+        return (
+            <div>
+                <h2>login</h2>
+                <form onSubmit={handleLogin}>
+                    <div>
+                        username
+                        <input
+                            type='text'
+                            value={username}
+                            name='Username'
+                            onChange={({ target }) => setUsername(target.value)}
+                        />
+                        <br></br>
+                        password
+                        <input
+                            type='text'
+                            value={password}
+                            name='Password'
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                    </div>
+                    <button type='submit'>login</button>
+                </form>
+            </div>
+        )
     }
 
     return (
         <div>
-            <h2>login</h2>
-            <form onSubmit={handleLogin}>
-                <div>
-                    username
-                    <input
-                        type='text'
-                        value={username}
-                        name='Username'
-                        onChange={({ target }) => setUsername(target.value)}
-                    />
-                    <br></br>
-                    password
-                    <input
-                        type='text'
-                        value={password}
-                        name='Password'
-                        onChange={({ target }) => setPassword(target.value)}
-                    />
-                </div>
-                <button type='submit'>login</button>
-            </form>
-
             <h2>blogs</h2>
             {blogs.map(blog => (
                 <Blog key={blog.id} blog={blog} />
