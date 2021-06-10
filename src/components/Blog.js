2576e9c84df959227837bf74c249ed76c69e0dbc
Brandon Warner <brandonswarner5@gmail.com>
2021-06-10 11:01:33 -0700

    can view all properties of blogs with button

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 const Blog = ({ blog }) => {
     // console.log('blog:', blog)
+    const [visible, setVisible] = useState(false)
+
+    const hideWhenVisible = { display: visible ? 'none' : '' }
+    const showWhenVisible = { display: visible ? '' : 'none' }
+
+    if (visible === false) {
+        return (
+            <div style={hideWhenVisible}>
+                {blog.title} {blog.author}
+                <button onClick={() => setVisible(true)}>view</button>
+            </div>
+        )
+    }
     return (
-        <div>
-            {blog.title} {blog.author}
+        <div style={showWhenVisible}>
+            {blog.title} {blog.author}{' '}
+            <button onClick={() => setVisible(false)}>hide</button>
+            <br></br>
+            {blog.url} <br></br>
+            likes: {blog.likes} <button>like</button>
+            <br></br>
+            {blog.user.name}
         </div>
     )
 }
