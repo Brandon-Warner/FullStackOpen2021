cda1899d81533c6b2734db790f43bdc67355aa64
Brandon Warner <brandonswarner5@gmail.com>
2021-06-16 13:01:00 -0700

    added test for url and likes to be shown after clicking view button

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders title and author', () => {
@@ -12,4 +12,26 @@ test('renders title and author', () => {
 
     expect(component.container).toHaveTextContent('blah blah blah')
     expect(component.container).toHaveTextContent('Dr Seuss')
+})
+
+test('clicking button shows like and url', () => {
+    const blog = {
+        title: 'blah blah bluh',
+        author: 'Dr Strange',
+        likes: 5,
+        url: 'www.blahblah.com',
+        user: {
+            name: 'Dave',
+        },
+    }
+
+    const mockHandler = jest.fn()
+
+    const component = render(<Blog blog={blog} setVisible={mockHandler} />)
+    const button = component.getByText('view')
+
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent('www.blahblah.com')
+    expect(component.container).toHaveTextContent('5')
 })
