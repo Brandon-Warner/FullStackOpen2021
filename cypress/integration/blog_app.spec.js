d61ee40c2969dd43c7115f46a536b5c32315d75d
Brandon Warner <brandonswarner5@gmail.com>
2021-06-22 16:40:29 -0700

    Exercise 5.22 complete. Testing for blogs in order of most likes

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -10,11 +10,11 @@ describe('Blog app', function () {
         cy.request('POST', 'http://localhost:3000/api/users', user)
         cy.visit('http://localhost:3000/')
     })
-    it('Login form is shown', function () {
+    it('login form is shown', function () {
         cy.contains('Please Log In')
         cy.contains('login')
     })
-    describe('Login', function () {
+    describe('login', function () {
         it('succeeds with valid credentials', function () {
             cy.contains('login').click()
             cy.get('.username').type('timmy')
@@ -24,7 +24,7 @@ describe('Blog app', function () {
             cy.contains('Timmy Turner is logged in')
         })
     })
-    describe('When logged in', function () {
+    describe('when logged in', function () {
         beforeEach(function () {
             cy.login({ username: 'timmy', password: 'turner' })
         })
@@ -38,7 +38,7 @@ describe('Blog app', function () {
 
             cy.contains('blogTitle')
         })
-        describe('When a blog already exists', function () {
+        describe('when a blog already exists', function () {
             beforeEach(function () {
                 cy.createBlog({
                     title: 'title1',
@@ -52,11 +52,53 @@ describe('Blog app', function () {
 
                 cy.contains('likes').contains('1')
             })
-            it.only('blog can only be deleted creator user', function () {
+            it('blog can only be deleted creator user', function () {
                 cy.contains('title1').contains('view').click()
                 cy.contains('title1').contains('remove').click()
                 cy.on('window:confirm', str => {
                     expect(str).to.eq('Are you sure you want to delete title1?')
+                })
+                cy.wait(2000)
+                cy.visit('http://localhost:3000/')
+            })
+            describe('when multiple blogs exist', function () {
+                beforeEach(function () {
+                    cy.createBlog({
+                        title: 'title2',
+                        author: 'author2',
+                        url: 'url2',
+                    })
+                    cy.createBlog({
+                        title: 'title3',
+                        author: 'author3',
+                        url: 'url3',
+                    })
+                })
+                it('blogs are in order of most to least likes', function () {
+                    cy.get('.blog')
+                        .find('button')
+                        .then(buttons => {
+                            cy.wrap(buttons[0]).click()
+                            cy.wrap(buttons[1]).click()
+                            cy.wrap(buttons[2]).click()
+                        })
+                    cy.wait(2000)
+                    cy.get('.like').then(buttons => {
+                        cy.wrap(buttons[0]).click()
+                        cy.wrap(buttons[1]).click()
+                        cy.wrap(buttons[2]).click()
+                        cy.wait(2000)
+                        cy.wrap(buttons[0]).click()
+                        cy.wrap(buttons[1]).click()
+                        cy.wait(2000)
+                        cy.wrap(buttons[0]).click()
+                        cy.wait(2000)
+                    })
+                    cy.get('.blog').then(blogs => {
+                        cy.wrap(blogs[0]).contains('likes').contains('3')
+                        cy.wrap(blogs[1]).contains('likes').contains('2')
+                        cy.wrap(blogs[2]).contains('likes').contains('1')
+                    })
                 })
             })
         })
