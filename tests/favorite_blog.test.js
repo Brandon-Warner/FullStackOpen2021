9812dbfe97808ce66268ce5402827a501548f393
Brandon Warner <brandonswarner5@gmail.com>
2021-06-23 12:05:40 -0700

    Part 5 Completed - backend

---
+++ tests/favorite_blog.test.js
@@ -0,0 +1,25 @@
+const listHelper = require('../utils/list_helper.js')
+
+describe('favorite blog', () => {
+    const blogs = [
+        {
+            title: 'React patterns',
+            author: 'Michael Chan',
+            likes: 7,
+        },
+        {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5,
+        },
+    ]
+
+    test('should be React Patterns', () => {
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toEqual({
+            title: 'React patterns',
+            author: 'Michael Chan',
+            likes: 7,
+        })
+    })
+})
