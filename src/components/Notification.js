50b4ce7886265d02075bb5501c29efd8daa8b179
Brandon Warner <brandonswarner5@gmail.com>
2021-07-20 11:42:32 -0700

    Notification component uses connect function instead of hooks   ex 6.19 completed

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,18 +1,24 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-    const notification = useSelector(state => state.notification)
-    // console.log('NOTIFICATION: ', notification)
+const Notification = props => {
     const style = {
         border: 'solid',
         padding: 10,
         borderWidth: 1,
     }
-    if (notification === '') {
+    if (props.notification === '') {
         return null
     }
-    return <div style={style}>{notification}</div>
+    return <div style={style}>{props.notification}</div>
 }
 
-export default Notification
+const mapStateToProps = state => {
+    return {
+        notification: state.notification,
+    }
+}
+
+const ConnectedNotes = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotes
