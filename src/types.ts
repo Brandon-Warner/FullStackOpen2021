c609d1c257b25c6d62c3cd483f008eec8f870cb5
Brandon Warner <brandonswarner5@gmail.com>
2021-11-17 12:46:14 -0800

    copied over type definitions from the backend

--- src/types.ts
+++ src/types.ts
@@ -5,14 +5,57 @@ export interface Diagnosis {
 }
 
 export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
 }
 
-export type Entry = {
-// eslint-disable-next-line @typescript-eslint/semi
+export type Discharge = {
+  date: string;
+  criteria: string;
+};
+
+export type SickLeave = {
+  startDate: string;
+  endDate: string;
+};
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
 }
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry = 
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 
 export interface Patient {
   id: string;
